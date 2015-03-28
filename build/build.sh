2eadeeb3aa578d83b8d6a8811772891fc63d2fda
Brandon Rhodes <brandon@rhodesmill.org>
2015-03-28 11:24:42 -0400

    Add shell script to fetch IMDB as .csv files

---
+++ build/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# All we want is the CSV files, but "imdbpy2sql.py" will not create them
+# unless it believes it can really talk to postgresql.
+
+wget -nc -nd -r -l 1 ftp://ftp.fu-berlin.de/pub/misc/movies/database/
+mkdir -p ../data
+imdbpy2sql.py -d . -u sqlite:../imdb.db -c ../data --csv-only-write
+chmod -w ../data/*.csv
+rm ../data/*.db
+
+#imdbpy2sql.py -d . -u postgres://host/database -c ../data --csv-only-write
