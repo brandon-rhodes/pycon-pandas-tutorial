4c64773311c13b3ad9c31c97d79f9a1614a6f798
Brandon Rhodes <brandon@rhodesmill.org>
2015-04-04 17:35:43 -0400

    Start of script to divide cells into sessions

---
+++ build/split.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+
+import json
+import os
+import re
+
+def main():
+    session_cells = {n: [] for n in range(1, 6+1)}
+    f = open(os.path.dirname(__file__) + '/../All.ipynb')
+    j = json.load(f)
+    cells = j['cells']
+    for cell in cells:
+        source = u''.join(cell['source'])
+        m = re.match(r'^# +(\d+)\.', source.strip())
+        if not m:
+            continue
+        n = int(m.group(1))
+        session_cells[n].append(cell)
+    for n, cells in sorted(session_cells.items()):
+        print 'Session {}: {} cells'.format(n, len(cells))
+
+if __name__ == '__main__':
+    main()
