325439fc61b89a61cdcb264d03f87ee97e5bd575
Brandon Rhodes <brandon@rhodesmill.org>
2017-07-19 06:39:36 -0400

    Adjust URL paths to IMDB databases
    Drat, it looks like it will soon require an S3 account to download the
    IMDB data files. Should I pivot to requiring every student to create an
    Amazon AWS account and giving them their credit card? For the moment, I
    am adjusting the URLs to refer to files that seem not to return 550
    errors. Fixes #32.

--- build/BUILD.sh
+++ build/BUILD.sh
@@ -3,9 +3,9 @@
 set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/actors.list.gz
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/actresses.list.gz
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/genres.list.gz
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/release-dates.list.gz
+curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/temporaryaccess/actors.list.gz
+curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/temporaryaccess/actresses.list.gz
+curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/temporaryaccess/genres.list.gz
+curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/temporaryaccess/release-dates.list.gz
 
 python ./BUILD.py
